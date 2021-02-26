89dab4222f2630923b9b199b46dd421f506d7df1
prak24oct <prakharatdit@gmail.com>
2021-02-26 13:47:03 +0530

    TrackEvent --> TrackierEvent

--- index.js
+++ index.js
@@ -8,13 +8,6 @@ import {
 
 const module_trackier = NativeModules.TrackierSDK;
 
-let module_trackier_emitter = null;
-if (Platform.OS === "android") {
-    module_trackier_emitter = new NativeEventEmitter(NativeModules.TrackierSDK);
-} else if (Platform.OS === "ios") {
-    
-}
-
 var TrackierConfig = function(appToken,environment){
     this.appToken = appToken ;
     this.environment = environment ;
@@ -26,11 +19,11 @@ TrackierSDK.initialize = function(trackierConfig){
     module_trackier.initializeSDK(trackierConfig)
 };
 
-TrackierSDK.trackEvent = function(trackEvent){
-    module_trackier.trackEvent(trackEvent)
+TrackierSDK.trackEvent = function(trackierEvent){
+    module_trackier.trackEvent(trackierEvent)
 };
 
-var TrackEvent = function(eventId) {
+var TrackierEvent = function(eventId) {
     this.eventId = eventId;
     this.orderId = null ;
     this.currency = null ;
@@ -49,23 +42,23 @@ var TrackEvent = function(eventId) {
     this.ev = {};
 }
 
-    TrackEvent.LEVEL_ACHIEVED = "1CFfUn3xEY"
-    TrackEvent.ADD_TO_CART = "Fy4uC1_FlN"
-    TrackEvent.ADD_TO_WISHLIST = "AOisVC76YG"
-    TrackEvent.COMPLETE_REGISTRATION = "mEqP4aD8dU"
-    TrackEvent.TUTORIAL_COMPLETION = "99VEGvXjN7"
-    TrackEvent.PURCHASE = "Q4YsqBKnzZ"
-    TrackEvent.SUBSCRIBE = "B4N_In4cIP"
-    TrackEvent.START_TRIAL = "jYHcuyxWUW"
-    TrackEvent.ACHIEVEMENT_UNLOCKED = "xTPvxWuNqm"
-    TrackEvent.CONTENT_VIEW = "Jwzois1ays"
-    TrackEvent.TRAVEL_BOOKING = "yP1-ipVtHV"
-    TrackEvent.SHARE = "dxZXGG1qqL"
-    TrackEvent.INVITE = "7lnE3OclNT"
-    TrackEvent.LOGIN = "o91gt1Q0PK"
-    TrackEvent.UPDATE = "sEQWVHGThl"
+    TrackierEvent.LEVEL_ACHIEVED = "1CFfUn3xEY"
+    TrackierEvent.ADD_TO_CART = "Fy4uC1_FlN"
+    TrackierEvent.ADD_TO_WISHLIST = "AOisVC76YG"
+    TrackierEvent.COMPLETE_REGISTRATION = "mEqP4aD8dU"
+    TrackierEvent.TUTORIAL_COMPLETION = "99VEGvXjN7"
+    TrackierEvent.PURCHASE = "Q4YsqBKnzZ"
+    TrackierEvent.SUBSCRIBE = "B4N_In4cIP"
+    TrackierEvent.START_TRIAL = "jYHcuyxWUW"
+    TrackierEvent.ACHIEVEMENT_UNLOCKED = "xTPvxWuNqm"
+    TrackierEvent.CONTENT_VIEW = "Jwzois1ays"
+    TrackierEvent.TRAVEL_BOOKING = "yP1-ipVtHV"
+    TrackierEvent.SHARE = "dxZXGG1qqL"
+    TrackierEvent.INVITE = "7lnE3OclNT"
+    TrackierEvent.LOGIN = "o91gt1Q0PK"
+    TrackierEvent.UPDATE = "sEQWVHGThl"
 
-TrackEvent.prototype.setEventValue = function(key, value) {
+TrackierEvent.prototype.setEventValue = function(key, value) {
     if (typeof key !== 'string' || typeof value !== 'string') {
         return;
     }
@@ -75,5 +68,5 @@ TrackEvent.prototype.setEventValue = function(key, value) {
 module.exports = {
     TrackierConfig,
     TrackierSDK,
-    TrackEvent,
+    TrackierEvent,
 }
