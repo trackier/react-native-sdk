4e6d28bdcc871c9741acc6d90156b59935b0a2ae
prak24oct <prakharatdit@gmail.com>
2021-03-01 15:38:21 +0530

    fix: package import

--- react-native.config.js
+++ react-native.config.js
@@ -4,7 +4,7 @@ module.exports = {
   dependency: {
     platforms: {
       android: {
-        packageImportPath: 'import com.trackier.sdk.TrackierPackage;',
+        packageImportPath: 'import com.trackier.reactsdk.TrackierPackage;',
         packageInstance: 'new TrackierPackage()',
       },
     },
