0e70ea8e11480f85c4a4fb67345e8fc495b5d274
prak24oct <prakharatdit@gmail.com>
2021-03-02 16:38:08 +0530

    Example App Updated

---
+++ example/App.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  TouchableHighlight
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import { TrackierConfig, TrackierSDK, TrackierEvent} from 'react-native-trackier-sdk';
+import { Adjust, AdjustEvent, AdjustConfig } from 'react-native-adjust';
+
+const App: () => React$Node = () => {
+  // var trackierConfig = new TrackierConfig("abcf2270-d94a-4364-a2ae-34903c6e1d53",TrackierConfig.EnvironmentDevelopment);
+  // TrackierSDK.initialize(trackierConfig);
+    const adjustConfig = new AdjustConfig("{YourAppToken}", AdjustConfig.EnvironmentSandbox);
+    Adjust.create(adjustConfig);
+
+  console.log("hey................");
+
+  function _onPress_trackSimpleEvent(){
+    // var trackierEvent = new TrackierEvent(TrackierEvent.UPDATE);
+    // trackierEvent.param1 = "XXXXXX";
+    // trackierEvent.param2 = "kkkkkkk";
+    // TrackierSDK.trackEvent(trackierEvent);
+  }
+  return (
+    <>
+      <View style={styles.container}>
+        <TouchableHighlight
+          style={styles.button}
+          onPress={_onPress_trackSimpleEvent}>
+          <Text>Track Simple Event</Text>
+        </TouchableHighlight>
+        </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#61D4FB',
+    padding: 10,
+    width: '60%',
+    height: 40,
+    margin: 10,
+  },
+});
+
+export default App;
