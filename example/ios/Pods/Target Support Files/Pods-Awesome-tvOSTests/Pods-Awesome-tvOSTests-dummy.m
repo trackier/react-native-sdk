719ed2aa402a329e6ce45fc5340a8877f264f071
Prakhar Srivastava <prakharatdit@gmail.com>
2021-04-24 18:34:52 +0530

    ios module added

--- example/ios/Pods/Target Support Files/Pods-Awesome-tvOSTests/Pods-Awesome-tvOSTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Awesome_tvOSTests : NSObject
-@end
-@implementation PodsDummy_Pods_Awesome_tvOSTests
-@end
--- example/ios/Pods/Target Support Files/Pods-AwesomeTests/Pods-AwesomeTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AwesomeTests : NSObject
-@end
-@implementation PodsDummy_Pods_AwesomeTests
-@end
