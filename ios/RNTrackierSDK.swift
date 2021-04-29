344977b308746a1340377404161fb68202ce679f
Prakhar Srivastava <prakharatdit@gmail.com>
2021-04-29 12:09:38 +0530

    Update RNTrackierSDK.swift

--- ios/RNTrackierSDK.swift
+++ ios/RNTrackierSDK.swift
@@ -13,10 +13,11 @@ import trackier_ios_sdk
 class RNTrackierSDK: NSObject {
   
   @objc func initializeSDK(_ dict: NSDictionary) -> Void {
+
+    NSLog("initializeSDK  dict %@ ", dict);
     
     let RNappToken = dict["appToken"] as! String;
     let RNenvironment = dict["environment"] as! String;
-    NSLog("inside swift module initializeSDK %@  and  %@",RNappToken,RNenvironment);
     
     let config = TrackierSDKConfig(appToken: RNappToken , env: RNenvironment)
     TrackierSDK.initialize(config: config)
