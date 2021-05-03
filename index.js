85f6cbd27d3fa851abaf8fea666e510986499b6f
Prakhar Srivastava <prakharatdit@gmail.com>
2021-05-03 15:45:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,18 +19,18 @@ TrackierSDK.initialize = function(trackierConfig) {
 
 TrackierSDK.trackEvent = function(trackierEvent) {
 	console.log("trackierEvent[v]................start");
-	let isValidArgs = true;
-	let props = ['eventId', 'orderId', 'currency', 'param1', 'param2', 'param3', 'param4', 'param5', 'param6', 'param7', 'param8', 'param9', 'param10'];
-	props.forEach(function (v, k) {
-		console.log("trackierEvent[v]................", trackierEvent[v]);
-		if (typeof trackierEvent[v] != 'undefined' && typeof trackierEvent[v] != 'string') {
-			isValidArgs = false;
-			return false;
-		}
-	})
-	if (!isValidArgs || typeof trackierEvent.revenue !== 'number') {
-		return;
-	}
+	// let isValidArgs = true;
+	// let props = ['eventId', 'orderId', 'currency', 'param1', 'param2', 'param3', 'param4', 'param5', 'param6', 'param7', 'param8', 'param9', 'param10'];
+	// props.forEach(function (v, k) {
+	// 	console.log("trackierEvent[v]................", trackierEvent[v]);
+	// 	if (typeof trackierEvent[v] != 'undefined' && typeof trackierEvent[v] != 'string') {
+	// 		isValidArgs = false;
+	// 		return false;
+	// 	}
+	// })
+	// if (!isValidArgs || typeof trackierEvent.revenue !== 'number') {
+	// 	return;
+	// }
 	module_trackier.trackEvent(trackierEvent)
 	console.log("trackierEvent[v]................end");
 };
