3a64fb15eecfbbbed475872764ab2cb7cecb39ad
Hemant Mann <hemant.mann121@gmail.com>
2021-05-03 12:44:49 +0530

    fix: index.js

--- index.js
+++ index.js
@@ -1,90 +1,85 @@
 'use strict';
 
 import { 
-    NativeModules
+	NativeModules
 } from 'react-native';
 
 const module_trackier = NativeModules.TrackierSDK;
 
-var TrackierConfig = function(appToken,environment){
-    this.appToken = appToken ;
-    this.environment = environment ;
+var TrackierConfig = function(appToken,environment) {
+	this.appToken = appToken;
+	this.environment = environment;
 }
 
 var TrackierSDK = {};
 
-TrackierSDK.initialize = function(trackierConfig){
-    module_trackier.initializeSDK(trackierConfig)
+TrackierSDK.initialize = function(trackierConfig) {
+	module_trackier.initializeSDK(trackierConfig)
 };
 
-TrackierSDK.trackEvent = function(trackierEvent){
-    if(typeof trackierEvent.eventId !== 'string'||
-    typeof trackierEvent.orderId !== 'string'||
-    typeof trackierEvent.currency !== 'string'||
-    typeof trackierEvent.param1 !== 'string'||
-    typeof trackierEvent.param2 !== 'string'||
-    typeof trackierEvent.param3 !== 'string'||
-    typeof trackierEvent.param4 !== 'string'||
-    typeof trackierEvent.param5 !== 'string'||
-    typeof trackierEvent.param6 !== 'string'||
-    typeof trackierEvent.param7 !== 'string'||
-    typeof trackierEvent.param8 !== 'string'||
-    typeof trackierEvent.param9 !== 'string'||
-    typeof trackierEvent.param10 !== 'string'||
-    typeof trackierEvent.revenue !== 'number') {
-        return;
-    }
-    module_trackier.trackEvent(trackierEvent)
+TrackierSDK.trackEvent = function(trackierEvent) {
+	let isValidArgs = true;
+	let props = ['eventId', 'orderId', 'currency', 'param1', 'param2', 'param3', 'param4', 'param5', 'param6', 'param7', 'param8', 'param9', 'param10'];
+	props.forEach(function (k, v) {
+		if (typeof trackierEvent[v] != 'undefined' && typeof trackierEvent[v] != 'string') {
+			isValidArgs = false;
+			return false;
+		}
+	})
+	if (!isValidArgs || typeof trackierEvent.revenue !== 'number') {
+		return;
+	}
+	module_trackier.trackEvent(trackierEvent)
 };
 
 TrackierConfig.EnvironmentDevelopment = "development";
 TrackierConfig.EnvironmentProduction = "production";
 
 var TrackierEvent = function(eventId) {
-    this.eventId = eventId;
-    this.orderId = null ;
-    this.currency = null ;
-    this.param1 = null ;
-    this.param2 = null ;
-    this.param3 = null ;
-    this.param4 = null ;
-    this.param5 = null ;
-    this.param6 = null ;
-    this.param7 = null ;
-    this.param8 = null ;
-    this.param9 = null ;
-    this.param10 = null ;
-    this.revenue = null ;
+	this.eventId = eventId;
+	this.orderId = null;
+	this.currency = null;
+	this.param1 = null;
+	this.param2 = null;
+	this.param3 = null;
+	this.param4 = null;
+	this.param5 = null;
+	this.param6 = null;
+	this.param7 = null;
+	this.param8 = null;
+	this.param9 = null;
+	this.param10 = null;
+	this.revenue = null;
 
-    this.ev = {};
+	this.ev = {};
 }
 
-    TrackierEvent.LEVEL_ACHIEVED = "1CFfUn3xEY"
-    TrackierEvent.ADD_TO_CART = "Fy4uC1_FlN"
-    TrackierEvent.ADD_TO_WISHLIST = "AOisVC76YG"
-    TrackierEvent.COMPLETE_REGISTRATION = "mEqP4aD8dU"
-    TrackierEvent.TUTORIAL_COMPLETION = "99VEGvXjN7"
-    TrackierEvent.PURCHASE = "Q4YsqBKnzZ"
-    TrackierEvent.SUBSCRIBE = "B4N_In4cIP"
-    TrackierEvent.START_TRIAL = "jYHcuyxWUW"
-    TrackierEvent.ACHIEVEMENT_UNLOCKED = "xTPvxWuNqm"
-    TrackierEvent.CONTENT_VIEW = "Jwzois1ays"
-    TrackierEvent.TRAVEL_BOOKING = "yP1-ipVtHV"
-    TrackierEvent.SHARE = "dxZXGG1qqL"
-    TrackierEvent.INVITE = "7lnE3OclNT"
-    TrackierEvent.LOGIN = "o91gt1Q0PK"
-    TrackierEvent.UPDATE = "sEQWVHGThl"
+TrackierEvent.LEVEL_ACHIEVED = "1CFfUn3xEY"
+TrackierEvent.ADD_TO_CART = "Fy4uC1_FlN"
+TrackierEvent.ADD_TO_WISHLIST = "AOisVC76YG"
+TrackierEvent.COMPLETE_REGISTRATION = "mEqP4aD8dU"
+TrackierEvent.TUTORIAL_COMPLETION = "99VEGvXjN7"
+TrackierEvent.PURCHASE = "Q4YsqBKnzZ"
+TrackierEvent.SUBSCRIBE = "B4N_In4cIP"
+TrackierEvent.START_TRIAL = "jYHcuyxWUW"
+TrackierEvent.ACHIEVEMENT_UNLOCKED = "xTPvxWuNqm"
+TrackierEvent.CONTENT_VIEW = "Jwzois1ays"
+TrackierEvent.TRAVEL_BOOKING = "yP1-ipVtHV"
+TrackierEvent.SHARE = "dxZXGG1qqL"
+TrackierEvent.INVITE = "7lnE3OclNT"
+TrackierEvent.LOGIN = "o91gt1Q0PK"
+TrackierEvent.UPDATE = "sEQWVHGThl"
 
 TrackierEvent.prototype.setEventValue = function(key, value) {
-    if (typeof key !== 'string') {
-        return;
-    }
-    this.ev[key] = value;
+	if (typeof key !== 'string') {
+		return;
+	}
+	this.ev[key] = value;
 };
 
 module.exports = {
-    TrackierConfig,
-    TrackierSDK,
-    TrackierEvent,
+	TrackierConfig,
+	TrackierSDK,
+	TrackierEvent,
 }
 
